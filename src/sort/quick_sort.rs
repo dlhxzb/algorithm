c418787a0a8fabc0f919f5eb208006ae36a6cfbb
dlhxzb <dlhxzb@163.com>
2022-11-30 15:16:58 +0800

    Refactor: optimize quick sort

--- src/sort/quick_sort.rs
+++ src/sort/quick_sort.rs
@@ -2,35 +2,22 @@ use std::fmt::Debug;
 
 // 递归版
 pub fn quick_sort<T: Ord + Debug>(list: &mut [T]) {
-    quick_sort_inner(list, 0, list.len() - 1);
-}
-
-fn quick_sort_inner<T: Ord + Debug>(list: &mut [T], start: usize, end: usize) {
-    if start >= end {
-        return;
-    }
-    if let Some(idx) = partition(list, start, end) {
-        if idx > start {
-            quick_sort_inner(list, start, idx - 1);
-        }
-        quick_sort_inner(list, idx + 1, end);
+    if let Some(idx) = partition(list) {
+        quick_sort(&mut list[..idx]);
+        quick_sort(&mut list[idx..]);
     }
 }
 
-// 迭代版
+// 迭代版，自上而下地分割排序
 pub fn quick_sort_no_recursion<T: Ord + Debug>(list: &mut [T]) {
-    let mut ranges = vec![(0, list.len() - 1)];
+    let mut ranges = vec![(0, list.len())];
+    // 因为不能[&mut list[..],&mut list[..]]，所以将分割后的数组范围存入ranges
     while !ranges.is_empty() {
         ranges = ranges
             .into_iter()
             .filter_map(|(start, end)| {
-                partition(list, start, end).map(|idx| {
-                    if idx > start {
-                        vec![(start, idx - 1), (idx + 1, end)]
-                    } else {
-                        vec![(idx + 1, end)]
-                    }
-                })
+                partition(&mut list[start..end])
+                    .map(|idx| [(start, start + idx), (start + idx, end)])
             })
             .flatten()
             .collect();
@@ -38,25 +25,30 @@ pub fn quick_sort_no_recursion<T: Ord + Debug>(list: &mut [T]) {
 }
 
 // 按首元素大小左右分割，返回分割后首元素位置
-fn partition<T: Ord + Debug>(list: &mut [T], start: usize, end: usize) -> Option<usize> {
-    if start >= end {
+fn partition<T: Ord + Debug>(list: &mut [T]) -> Option<usize> {
+    let len = list.len();
+    if len <= 1 {
         return None;
     }
-    let (mut i, mut j) = (start, end);
+    let (mut i, mut j) = (1, len - 1);
     while i < j {
-        // 从后向前找到第一个小于start的，到start为止
-        while list[j] >= list[start] && j > start {
-            j -= 1;
+        // [i] > [0] > [j]时，交换[i],[j]
+        if list[i] > list[0] && list[j] < list[0] {
+            list.swap(i, j);
         }
-        // 从前向后找到第一个大于start的，到j为止
-        while list[i] <= list[start] && i < j {
+        // [i]左侧都要<[0]
+        if list[i] < list[0] {
             i += 1;
         }
-        // 交换大于小于值，j没找到的话i=j=start，i没找到的话i=j
-        list.swap(i, j);
+        // [j]右侧都要>=[0]，避免i,j都不步进，卡在==[0]
+        if list[j] >= list[0] {
+            j -= 1;
+        }
     }
-    // [j]值<=start，就是start该替换的位置
-    list.swap(start, j);
+    // [j]值经过-=后不确定是否<[0]
+    if list[j] < list[0] {
+        list.swap(0, j);
+    }
     Some(j)
 }
 
