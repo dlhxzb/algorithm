721af4af0ff61b9fbfc1244768622c85f4fbb564
dlhxzb <dlhxzb@163.com>
2023-05-09 16:52:24 +0800

    Refactor: optimize quick sort

--- src/sort/heap_sort.rs
+++ src/sort/heap_sort.rs
@@ -1,9 +1,8 @@
-use std::collections::VecDeque;
 use std::fmt::Debug;
 
 // 为所有权转移到小顶堆方便，使用Vec非[]
 pub fn heap_sort<T: Ord + Debug>(list: &mut Vec<T>) {
-    let mut q = VecDeque::with_capacity(list.len());
+    let mut q = Vec::with_capacity(list.len());
     while let Some(data) = list.pop() {
         insert_end(&mut q, data);
     }
@@ -12,8 +11,8 @@ pub fn heap_sort<T: Ord + Debug>(list: &mut Vec<T>) {
     }
 }
 
-fn insert_end<T: Ord + Debug>(q: &mut VecDeque<T>, data: T) {
-    q.push_back(data);
+fn insert_end<T: Ord + Debug>(q: &mut Vec<T>, data: T) {
+    q.push(data);
     let mut new_idx = q.len() - 1;
     while new_idx > 0 {
         let parent = (new_idx - 1) / 2;
@@ -26,19 +25,20 @@ fn insert_end<T: Ord + Debug>(q: &mut VecDeque<T>, data: T) {
     }
 }
 
-fn delete_top<T: Ord + Debug>(q: &mut VecDeque<T>) -> Option<T> {
-    let top = q.pop_front();
-    // 首尾互换，避免移除顶部后窜行导致叶子节点比父节点大
-    if let Some(d) = q.pop_back() {
-        q.push_front(d);
+fn delete_top<T: Ord + Debug>(q: &mut Vec<T>) -> Option<T> {
+    if q.is_empty() {
+        return None;
     }
-    let len = q.len();
+    let len = q.len() - 1;
+    // 首尾互换，避免移除顶部后窜行导致叶子节点比父节点大
+    q.swap(0, len);
+    let top = q.pop();
     let mut idx = 0;
     // 左叶子节点存在
     while idx * 2 + 1 < len {
         let left = idx * 2 + 1;
         let right = left + 1;
-        if right < len && q[right] < q[left] {
+        if right < len && q[right] < q[left] && q[right] < q[idx] {
             q.swap(right, idx);
             idx = right;
         } else if q[left] < q[idx] {
--- src/sort/quick_sort.rs
+++ src/sort/quick_sort.rs
@@ -4,27 +4,23 @@ use std::fmt::Debug;
 pub fn quick_sort<T: Ord + Debug>(list: &mut [T]) {
     if let Some(idx) = partition(list) {
         quick_sort(&mut list[..idx]);
-        quick_sort(&mut list[idx..]);
+        quick_sort(&mut list[idx + 1..]);
     }
 }
 
 // 迭代版，自上而下地分割排序
 pub fn quick_sort_no_recursion<T: Ord + Debug>(list: &mut [T]) {
-    let mut ranges = vec![(0, list.len())];
-    // 因为不能[&mut list[..],&mut list[..]]，所以将分割后的数组范围存入ranges
-    while !ranges.is_empty() {
-        ranges = ranges
-            .into_iter()
-            .filter_map(|(start, end)| {
-                partition(&mut list[start..end])
-                    .map(|idx| [(start, start + idx), (start + idx, end)])
-            })
-            .flatten()
-            .collect();
+    let mut stack = vec![(0, list.len())];
+    // 因为不能[&mut list[..],&mut list[..]]，所以将分割后的数组范围存入stack
+    while let Some((start, end)) = stack.pop() {
+        if let Some(idx) = partition(&mut list[start..end]) {
+            stack.push((start, start + idx));
+            stack.push((start + idx + 1, end));
+        }
     }
 }
 
-// 按首元素大小左右分割，返回分割后首元素位置
+// 按首元素大小左右分割，左侧小于，右侧大于等于，返回分割后首元素位置，分割点不再参加排序
 fn partition<T: Ord + Debug>(list: &mut [T]) -> Option<usize> {
     let len = list.len();
     if len <= 1 {
@@ -32,23 +28,23 @@ fn partition<T: Ord + Debug>(list: &mut [T]) -> Option<usize> {
     }
     let (mut i, mut j) = (1, len - 1);
     while i < j {
-        // [i] > [0] > [j]时，交换[i],[j]
-        if list[i] > list[0] && list[j] < list[0] {
-            list.swap(i, j);
-        }
         // [i]左侧都要<[0]
         if list[i] < list[0] {
             i += 1;
+            continue;
         }
-        // [j]右侧都要>=[0]，避免i,j都不步进，卡在==[0]
+        // [j]右侧都要>=[0]
         if list[j] >= list[0] {
             j -= 1;
+            continue;
         }
+        list.swap(i, j);
     }
-    // [j]值经过-=后不确定是否<[0]
-    if list[j] < list[0] {
-        list.swap(0, j);
+    // 找到最后一个比[0]小的
+    if list[j] >= list[0] {
+        j -= 1;
     }
+    list.swap(0, j);
     Some(j)
 }
 
